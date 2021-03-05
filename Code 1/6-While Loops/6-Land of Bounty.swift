02c3c9e8fdc24471c1c77db51f9331c34ab34090
JaeHee-Jinn <78120266+JaeHee-Jinn@users.noreply.github.com>
2021-03-04 22:21:31 -0500

    Update 6-Land of Bounty.swift

--- Code 1/6-While Loops/6-Land of Bounty.swift
+++ Code 1/6-While Loops/6-Land of Bounty.swift
@@ -1,1 +1,27 @@
-//
+func solveRowOfSwitchesAndGems() {
+while !isBlocked {
+    
+        if isOnClosedSwitch {
+            toggleSwitch()
+        }else if isOnGem{
+            collectGem()
+
+        }
+    moveForward()
+    }
+}
+solveRowOfSwitchesAndGems()
+turnRight()
+moveForward()
+turnRight()
+solveRowOfSwitchesAndGems()
+turnLeft()
+moveForward()
+turnLeft()
+solveRowOfSwitchesAndGems()
+
+
+//This is great work JaeHee
+//You solved this by using divide and conquer!!.
+//WAY TO GO!
+//From Jae your teacher
