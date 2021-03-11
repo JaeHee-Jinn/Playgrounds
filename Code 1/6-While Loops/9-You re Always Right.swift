3fc8d13e9e0de7825a87577cb7cb10c6c6cc1e92
JaeHee-Jinn <78120266+JaeHee-Jinn@users.noreply.github.com>
2021-03-10 19:57:50 -0500

    Update 9-You re Always Right.swift

--- Code 1/6-While Loops/9-You re Always Right.swift
+++ Code 1/6-While Loops/9-You re Always Right.swift
@@ -1,1 +1,16 @@
-//
+func oneLineSolved() {
+    while !isBlocked {
+        moveForward()
+        if isOnClosedSwitch {
+        toggleSwitch()
+        }
+        if isOnGem {
+            collectGem()
+        }
+    }
+    turnRight()
+    
+}
+for i in 1 ... 5 {
+    oneLineSolved()
+}
