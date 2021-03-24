9276fad4c4821ca1b8d940949a35c20005a2a7c4
JaeHee-Jinn <78120266+JaeHee-Jinn@users.noreply.github.com>
2021-03-24 19:56:18 -0400

    Update 1-The Right-Hand Rule.swift
    This is my first Algorithm project and I also learned about pseudocode, code but with everyday human language.😁😇🐶

--- Code 1/7-Algorithms/1-The Right-Hand Rule.swift
+++ Code 1/7-Algorithms/1-The Right-Hand Rule.swift
@@ -1,1 +1,35 @@
-//
+func navigateAroundWall(){
+    if isBlockedRight{
+        moveForward()
+    }else {
+        turnRight()
+        moveForward()
+    }
+}
+while !isOnClosedSwitch {
+    navigateAroundWall()
+    if isOnGem {
+        collectGem()
+        turnRight()
+        turnRight()
+    }
+}
+toggleSwitch()
+    /*
+func navigateAroundWall() {
+    if isBlockedRight {
+        moveForward()
+    }  else {
+        turnRight()
+        moveForward()
+        if isOnGem {
+            collectGem()
+        }
+        if isOnClosedSwitch {
+            toggleSwitch()
+        }
+        
+    }
+    
+}
+ */
