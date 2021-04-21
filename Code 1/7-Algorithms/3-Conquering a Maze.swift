be34832f29561d162a0d471bae22aa8c4f0caed6
JaeHee-Jinn <78120266+JaeHee-Jinn@users.noreply.github.com>
2021-04-21 19:47:11 -0400

    Update 3-Conquering a Maze.swift

--- Code 1/7-Algorithms/3-Conquering a Maze.swift
+++ Code 1/7-Algorithms/3-Conquering a Maze.swift
@@ -1,1 +1,15 @@
-//
+func navigateAroundWall() {
+    if isBlocked && isBlockedRight {
+        turnLeft()
+    }else if isBlockedRight {
+        moveForward()
+    }else {
+        turnRight()
+        moveForward()
+    }
+}
+
+while !isOnGem {
+    navigateAroundWall()
+}
+collectGem()
