0b36dbaa3701bfc238026fbd03aa83c36321e7f8
JaeHee-Jinn <78120266+JaeHee-Jinn@users.noreply.github.com>
2021-05-05 19:46:59 -0400

    The Maze of Mischeif
    The pseudocode is added making it a full project plus the pseudocode.Almost to code 2!

--- Code 1/7-Algorithms/3-Conquering a Maze.swift
+++ Code 1/7-Algorithms/3-Conquering a Maze.swift
@@ -1,3 +1,14 @@
+Pseudocode ex.
+/*
+ recall the previous function on navigating around the wall
+ if you are blocked and you are blocked on the right turn left otherwise if you're blocked just on the right then you shall move 
+forward else turn right and move forward
+
+
+*/
+
+
+
 func navigateAroundWall() {
     if isBlocked && isBlockedRight {
         turnLeft()
