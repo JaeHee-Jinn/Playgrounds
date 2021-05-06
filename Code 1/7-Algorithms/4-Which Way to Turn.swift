29aa9e5ca23aefe5f1f46be23fad4854045f0d8a
JaeHee-Jinn <78120266+JaeHee-Jinn@users.noreply.github.com>
2021-05-05 20:18:36 -0400

    Labyrinthine Twisting Turn
    I'M ON MY LAST PUZZLE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- Code 1/7-Algorithms/4-Which Way to Turn.swift
+++ Code 1/7-Algorithms/4-Which Way to Turn.swift
@@ -1,1 +1,20 @@
-//
+
+/*
+Pseudocode
+
+while byte isn't on a gem continue to solve the puzzle
+ check if you're on a closed switch
+ and if you're on one than close the switch
+ if you're on a closed switch and blocked on the front than turn left and continue solving the puzzle
+ if not blocked on the front than toggle switch and turn right
+ */
+while !isOnGem {
+    if isOnClosedSwitch && isBlocked{
+        turnLeft()
+        toggleSwitch()
+    }else if !isBlocked && isOnClosedSwitch{
+        toggleSwitch()
+        turnRight()
+    }
+    moveForward()
+
