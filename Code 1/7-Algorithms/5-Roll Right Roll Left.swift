32a57c9352d9b999fe6f0adf4137a3284866e530
JaeHee-Jinn <78120266+JaeHee-Jinn@users.noreply.github.com>
2021-05-12 20:32:35 -0400

    Update 5-Roll Right Roll Left.swift
    UNFINISHED

--- Code 1/7-Algorithms/5-Roll Right Roll Left.swift
+++ Code 1/7-Algorithms/5-Roll Right Roll Left.swift
@@ -1,1 +1,26 @@
-//
+UNFINISHED
+
+
+    
+ func collectingGemsInOneRow(){
+ for i in 1 ... 2{
+ collectGem()
+ moveForward()
+ }
+    while !isBlocked {
+        moveForward()
+    }
+ }
+ 
+/*
+continue moving foward until you are blocked
+ when you are blocked then turn left
+ */
+ 
+ 
+ while !isOnOpenSwitch {
+ moveForward()
+ turnRight()
+ collectingGemsInOneRow()
+ 
+ }
