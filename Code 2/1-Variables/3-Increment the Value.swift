1e5363e63a21ec2c749441a147abc34811afcab5
JaeHee-Jinn <78120266+JaeHee-Jinn@users.noreply.github.com>
2021-06-05 11:06:31 -0400

    Update 3-Increment the Value.swift

--- Code 2/1-Variables/3-Increment the Value.swift
+++ Code 2/1-Variables/3-Increment the Value.swift
@@ -1,1 +1,18 @@
+var gemCounter = 0
+func increaseTheGemCounter() {
+    while !isBlocked {
+        moveForward()
+        if isOnGem {
+            collectGem()
+            gemCounter += 1
+        }
+    }
+}
+func turnTheCorner() {
+    increaseTheGemCounter()
+    turnRight()
+}
 
+turnTheCorner()
+turnTheCorner()
+increaseTheGemCounter()
