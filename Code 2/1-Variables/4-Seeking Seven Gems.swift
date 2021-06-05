7c46d186693fa539e5a0c896034fdba4fd2d7b60
JaeHee-Jinn <78120266+JaeHee-Jinn@users.noreply.github.com>
2021-06-05 11:07:06 -0400

    Update 4-Seeking Seven Gems.swift

--- Code 2/1-Variables/4-Seeking Seven Gems.swift
+++ Code 2/1-Variables/4-Seeking Seven Gems.swift
@@ -1,1 +1,14 @@
-
+var gemCounter = 0
+while gemCounter < 7 {
+    if isOnGem {
+        collectGem()
+        gemCounter += 1
+    }
+    moveForward()
+    if isBlocked {
+        turnRight()
+        turnRight()
+    }
+    
+    
+}
