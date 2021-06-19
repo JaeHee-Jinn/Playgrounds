1e25e935e52ee22bdbd6c4281f5d00ce44fde22b
JaeHee-Jinn <78120266+JaeHee-Jinn@users.noreply.github.com>
2021-06-19 10:18:00 -0400

    Update 5-Three Gems Four Switches.swift

--- Code 2/1-Variables/5-Three Gems Four Switches.swift
+++ Code 2/1-Variables/5-Three Gems Four Switches.swift
@@ -1,1 +1,64 @@
+var gemCounter = 0
+var switchCounter = 0
+for i in 1 ... 15 {
+    if isBlocked && isBlockedRight {
+        turnLeft()
+    }else if isBlocked && isBlockedLeft {
+        turnRight()
+    }
+    if isOnGem {
+        if gemCounter < 3 {
+            collectGem()
+            gemCounter += 1
+        }
 
+    }
+    moveForward()
+    
+}
+for i in 1 ... 10 {
+    if isBlocked && isBlockedRight {
+        turnLeft()
+    }else if isBlocked && isBlockedLeft {
+        turnRight()
+    }
+    if isOnClosedSwitch {
+        if switchCounter < 4 {
+            toggleSwitch()
+            switchCounter += 1
+        }
+        
+    }
+    moveForward()
+    
+}
+
+
+/*
+ collect  3  gems and toggle 4 switche
+ 
+ while numberOGems < 3{
+ 
+ if blocked and blocked right
+ turn left
+ else if blocked and blocked left
+ turn right
+ if on gem 
+ collect gem
+ gem counter += 1
+ moveForward()
+ 
+ }
+ 
+ while numberOfSwitches  < 4{
+ if blocked and blocked right
+ turn left
+ else if blocked and blocked left
+ turn right
+ if on closed switch
+ toggle switch
+ switchcounter += 1
+ moveForward()
+ 
+ }
+ */
