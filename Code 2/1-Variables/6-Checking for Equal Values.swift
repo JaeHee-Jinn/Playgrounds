e27c146aa6271ef25902a8bcbd3d0d8f2f00ee99
JaeHee-Jinn <78120266+JaeHee-Jinn@users.noreply.github.com>
2021-07-03 10:21:23 -0400

    Update 6-Checking for Equal Values.swift
    Easy peasy, lemon squeezy.

--- Code 2/1-Variables/6-Checking for Equal Values.swift
+++ Code 2/1-Variables/6-Checking for Equal Values.swift
@@ -1,1 +1,8 @@
-//
+let switchCounter = numberOfSwitches var gemCounter = 0 
+while gemCounter != numberOfSwitches { 
+  if isOnGem { 
+ collectGem() 
+ gemCounter += 1 }
+  moveForward() 
+  if isBlocked { 
+    turnRight() }
