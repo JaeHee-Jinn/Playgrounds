a50e0e670ace837db600cbd89216fcacf041bf26
JaeHee-Jinn <78120266+JaeHee-Jinn@users.noreply.github.com>
2021-07-17 10:31:29 -0400

    Update 1-Deactivating a Portal.swift

--- Code 2/2-Types/1-Deactivating a Portal.swift
+++ Code 2/2-Types/1-Deactivating a Portal.swift
@@ -1,1 +1,22 @@
-
+greenPortal.isActive = false
+for i in 1 ... 6 {
+    moveForward()
+}
+toggleSwitch()
+turnLeft()
+turnLeft()
+func moveThree() {
+    for i in 1 ... 3 {
+    moveForward()
+}
+}
+moveThree()
+turnRight()
+moveThree()
+toggleSwitch()
+turnLeft()
+turnLeft()
+for i in 1 ... 6 {
+    moveForward()
+}
+toggleSwitch()
