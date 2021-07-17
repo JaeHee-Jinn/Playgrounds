3c22342ff6a711b61d3b1b7f7bf27bbf34e57308
JaeHee-Jinn <78120266+JaeHee-Jinn@users.noreply.github.com>
2021-07-17 10:30:57 -0400

    Update 8-Collect the Total.swift

--- Code 2/1-Variables/8-Collect the Total.swift
+++ Code 2/1-Variables/8-Collect the Total.swift
@@ -1,1 +1,17 @@
-
+var gemCounter = 0
+let totalGems = randomNumberOfGems
+ 
+ while gemCounter !=  totalGems{
+ moveForward()
+ if isOnGem {
+ collectGem()
+ gemCounter += 1
+ }
+ if isBlocked && isBlockedLeft {
+ turnRight()
+ }
+    if isBlocked && isBlockedRight {
+        turnLeft()
+    }
+ }
+ 
