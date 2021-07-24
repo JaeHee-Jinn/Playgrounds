a1ca54ea683e2025f90981650416efa4510cf882
JaeHee-Jinn <78120266+JaeHee-Jinn@users.noreply.github.com>
2021-07-24 10:22:30 -0400

    Update 2-Portal On and Off.swift

--- Code 2/2-Types/2-Portal On and Off.swift
+++ Code 2/2-Types/2-Portal On and Off.swift
@@ -1,1 +1,33 @@
-
+purplePortal.isActive = false
+for i in 1 ... 6 {
+    moveForward()
+    if isOnGem {
+        collectGem()
+    }
+}
+func turnAround() {
+    turnRight()
+    turnRight()
+}
+turnAround()
+for i in 1 ... 3{
+    moveForward()
+}
+purplePortal.isActive = true
+moveForward()
+turnAround()
+for i in 1 ... 3 {
+    moveForward()
+    if isOnGem {
+        collectGem()
+    }
+}
+turnAround()
+for i in 1 ... 2 {
+    moveForward()
+}
+purplePortal.isActive = false
+for i in 1 ... 3 {
+    moveForward()
+}
+toggleSwitch()
