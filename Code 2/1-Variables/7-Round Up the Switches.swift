c8eb3d366088d58050cbd34afe0621c7e225f68c
JaeHee-Jinn <78120266+JaeHee-Jinn@users.noreply.github.com>
2021-07-31 10:07:28 -0400

    Update 7-Round Up the Switches.swift

--- Code 2/1-Variables/7-Round Up the Switches.swift
+++ Code 2/1-Variables/7-Round Up the Switches.swift
@@ -1,1 +1,50 @@
-//
+var gemCounter = 0
+var switchCounter = 0
+        for i in 1 ... 9 {
+            moveForward()
+            if isBlocked {
+                turnRight()
+            }
+            if isOnGem {
+                collectGem()
+                gemCounter += 1
+            }
+        }
+while switchCounter != gemCounter {
+    moveForward()
+    if isBlocked {
+        turnRight()
+    }
+    if isOnClosedSwitch {
+        toggleSwitch()
+        switchCounter += 1
+    }
+}
+
+
+/*
+ var gemCounter 
+ 
+ 
+ 
+ go around and count the number of gems
+ then go over to the switches
+ and do the same thing
+        move forward
+        move forward
+        
+
+        
+        for i in 1... 7{
+        if blocked then turen        
+        if isOnGem{
+        collectGem
+        addto gemcounter
+        }
+        }
+ 
+ 
+ 
+ 
+ 
+ */
