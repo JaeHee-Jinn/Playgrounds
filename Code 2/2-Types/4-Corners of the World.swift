9242c3cfc75122014372658cd9b416530e56656c
JaeHee-Jinn <78120266+JaeHee-Jinn@users.noreply.github.com>
2021-08-14 10:07:04 -0400

    Update 4-Corners of the World.swift

--- Code 2/2-Types/4-Corners of the World.swift
+++ Code 2/2-Types/4-Corners of the World.swift
@@ -1,1 +1,64 @@
-//
+func moveAndCollect() {
+    moveForward()
+    collectGem()
+}
+func moveLeft() {
+    moveForward()
+    turnLeft()
+}
+func moveTwice(){
+    moveForward()
+    moveForward()
+}
+turnRight()
+moveForward()
+orangePortal.isActive = false
+moveTwice()
+toggleSwitch()
+func turnAround() {
+    turnRight()
+    turnRight()
+}
+turnAround()
+moveLeft()
+moveForward()
+toggleSwitch()
+turnAround()
+orangePortal.isActive = true
+moveTwice()
+collectGem()
+turnAround()
+orangePortal.isActive = false
+moveLeft()
+for i in 1 ... 2{
+    moveAndCollect()
+    turnAround()
+    moveLeft()
+}
+moveAndCollect()
+turnAround()
+moveForward()
+turnRight()
+moveTwice()
+greenPortal.isActive = false
+moveTwice()
+collectGem()
+turnAround()
+moveLeft()
+moveAndCollect()
+turnAround()
+greenPortal.isActive = true
+moveTwice()
+greenPortal.isActive = false
+toggleSwitch()
+turnAround()
+moveTwice()
+toggleSwitch()
+turnAround()
+moveLeft()
+moveForward()
+toggleSwitch()
+turnAround()
+moveTwice()
+toggleSwitch()
+
