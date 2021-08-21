45c103f51905b2aa6f3350ae61b310f8d3eb0e54
JaeHee-Jinn <78120266+JaeHee-Jinn@users.noreply.github.com>
2021-08-21 10:07:46 -0400

    Update 5-Random Gems Everywhere.swift

--- Code 2/2-Types/5-Random Gems Everywhere.swift
+++ Code 2/2-Types/5-Random Gems Everywhere.swift
@@ -1,1 +1,77 @@
+let totalGems = randomNumberOfGems
+func turnAround() {
+    turnRight()
+    turnRight()
+}
+moveForward()
+for i in 1 ... 2 {
+    moveForward()
+    if isOnGem {
+        collectGem()
+    }
+}
+turnAround()
+pinkPortal.isActive = false
+for i in 1 ... 4 {
+    moveForward()
+    if isOnGem {
+        collectGem()
+    }
+}
+turnAround()
+moveForward()
+pinkPortal.isActive = true
+moveForward()
+moveForward()
+func checkLeftRight() {
+    turnRight()
+    moveForward()
+    if isOnGem {
+        collectGem()
+    }
+    turnAround()
+    for i in 1 ... 2 {
+        moveForward()
+        if isOnGem {
+            collectGem()
+        }
+    }
+}
+checkLeftRight()
+turnAround()
+moveForward()
+turnRight()
+pinkPortal.isActive = false
+bluePortal.isActive = false
+for i in 1 ... 4 {
+    moveForward()
+}
+checkLeftRight()
+turnAround()
+moveForward()
+turnRight()
+bluePortal.isActive = true
+moveForward()
+for i in 1 ... 2 {
+    moveForward()
+    if isOnGem {
+        collectGem()
+    }
+}
+turnAround()
+bluePortal.isActive = false
+for i in 1 ... 4 {
+    moveForward()
+    if isOnGem {
+        collectGem()
+    }
+}
+/*
+ go through portal
+ collect gems in line
+ go through portal again
+ check gems on 7 places
+ go through blue portal
+ finish by collecting gems in last row
+  */
 
