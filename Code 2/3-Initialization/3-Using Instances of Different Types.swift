d7ca0467839ddd60f1d7e00a47f72be33eacb55b
JaeHee-Jinn <78120266+JaeHee-Jinn@users.noreply.github.com>
2021-09-11 10:27:23 -0400

    Update 3-Using Instances of Different Types.swift

--- Code 2/3-Initialization/3-Using Instances of Different Types.swift
+++ Code 2/3-Initialization/3-Using Instances of Different Types.swift
@@ -1,1 +1,16 @@
-
+expert.moveForward()
+expert.turnLockUp()
+for i in 1 ... 2 {
+    character.moveForward()
+    if character.isOnGem {
+        character.collectGem()
+    }
+}
+character.turnRight()
+character.moveForward()
+character.moveForward()
+for i in 1 ... 2 {
+    expert.turnLockDown()
+}
+character.moveForward()
+character.collectGem()
