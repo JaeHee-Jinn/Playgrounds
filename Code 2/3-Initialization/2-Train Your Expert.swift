7170967a5fcc8999cc9424f36918902cde9ccd07
JaeHee-Jinn <78120266+JaeHee-Jinn@users.noreply.github.com>
2021-09-11 10:06:26 -0400

    Update 2-Train Your Expert.swift

--- Code 2/3-Initialization/2-Train Your Expert.swift
+++ Code 2/3-Initialization/2-Train Your Expert.swift
@@ -1,1 +1,44 @@
+func turnAround() {
+    expert.turnRight()
+    expert.turnRight()
+}
+func moveTwice() {
+    expert.moveForward()
+    expert.moveForward()
+}
 
+turnAround()
+moveTwice()
+expert.collectGem()
+expert.turnLockDown()
+turnAround()
+moveTwice()
+expert.turnRight()
+moveTwice()
+moveTwice()
+expert.moveForward()
+expert.collectGem()
+turnAround()
+moveTwice()
+expert.turnRight()
+moveTwice()
+expert.collectGem()
+turnAround()
+func moveTwiceTwice() {
+    for i in 1 ... 2 {
+        moveTwice()
+    }
+}
+moveTwiceTwice()
+expert.collectGem()
+turnAround()
+moveTwice()
+expert.turnLeft()
+moveTwiceTwice()
+expert.moveForward()
+expert.collectGem()
+turnAround()
+moveTwice()
+expert.turnLeft()
+moveTwice()
+expert.collectGem()
