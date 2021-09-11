b58b959c3b05e0fbf2a0e95fe6d1ce33f54079a8
JaeHee-Jinn <78120266+JaeHee-Jinn@users.noreply.github.com>
2021-09-11 10:27:56 -0400

    Update 1-Initializing Your Expert.swift

--- Code 2/3-Initialization/1-Initializing Your Expert.swift
+++ Code 2/3-Initialization/1-Initializing Your Expert.swift
@@ -1,1 +1,26 @@
-
+for i in 1 ... 3 {
+    expert.moveForward()
+}
+expert.turnLockUp()
+expert.turnRight()
+expert.turnRight()
+for i in 1 ... 6 {
+    expert.moveForward()
+}
+expert.collectGem()
+expert.turnRight()
+expert.turnRight()
+for i in 1 ... 3 {
+    expert.moveForward()
+}
+expert.turnRight()
+for i in 1 ... 3 {
+    expert.moveForward()
+}
+expert.collectGem()
+expert.turnRight()
+expert.turnRight()
+for i in 1 ... 6 {
+    expert.moveForward()
+}
+expert.collectGem()
