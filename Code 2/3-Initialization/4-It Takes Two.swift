fb2afaa87abddba74529d5ef534400bdb1dc083c
JaeHee-Jinn <78120266+JaeHee-Jinn@users.noreply.github.com>
2021-09-18 09:34:39 -0400

    Update 4-It Takes Two.swift
    FINISHED.😭😭😭

--- Code 2/3-Initialization/4-It Takes Two.swift
+++ Code 2/3-Initialization/4-It Takes Two.swift
@@ -1,1 +1,38 @@
+let expert = Expert()
+let character = Character()
+expert.turnLeft()
+for i in 1 ... 3 {
+    expert.moveForward()
+}
+func moveTwice() {
+    expert.moveForward()
+    expert.moveForward()
+}
+func moveCharacter() {
+    character.moveForward()
+    character.moveForward()
+}
+expert.turnRight()
+moveTwice()
+expert.turnLeft()
+for i in 1 ... 2 {
+    expert.turnLockDown()
+}
+
+moveCharacter()
+character.collectGem()
+expert.turnRight()
+moveTwice()
+expert.turnRight()
+for i in 1 ... 6 {
+    expert.moveForward()
+}
+expert.turnRight()
+moveTwice()
+expert.turnLeft()
+expert.turnLockUp()
+moveCharacter()
+character.toggleSwitch()
+
+
 
