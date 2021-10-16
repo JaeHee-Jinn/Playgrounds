466524e0fdc0d0dbd5c36fbfca6185fe1cec8144
JaeHee-Jinn <78120266+JaeHee-Jinn@users.noreply.github.com>
2021-10-16 10:36:19 -0400

    Update 2-Generalizing a Function.swift

--- Code 2/4-Parameters/2-Generalizing a Function.swift
+++ Code 2/4-Parameters/2-Generalizing a Function.swift
@@ -1,15 +1,77 @@
-move(distance: 6)
-expert.turnRight()
-move(distance: 2)
-expert.turnRight()
-move(distance: 5)
-expert.turnLeft()
-move(distance: 5)
-expert.turnLockUp()
-expert.turnLeft()
 for i in 1 ... 2 {
-move(distance: 3)
-expert.turnRight()
+    for i in 1 ... 2 {
+    expert.turnRight()
 }
-move(distance: 4)
-expert.collectGem()
+turnLock(up: true, numberOfTimes: 3)
+}
+for i in 1 ... 3 {
+    character.moveForward()
+}
+character.collectGem()
+for i in 1 ... 2 {
+    character.turnRight()
+}
+for i in 1 ... 2 {
+    character.moveForward()
+    character.turnLeft()
+}
+
+turnLock(up: false, numberOfTimes: 2)
+character.moveForward()
+character.collectGem()
+for i in 1 ... 2 {
+    expert.turnRight()
+    }
+turnLock(up: false, numberOfTimes: 3)
+expert.turnRight()
+expert.turnRight()
+character.turnRight()
+character.turnRight()
+character.moveForward()
+turnLock(up: false, numberOfTimes: 1)
+for i in 1 ... 2 {
+    character.moveForward()
+}
+character.collectGem()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
